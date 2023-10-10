61471bf97ad0d96836b2226026deed67a793cf55
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-10-10 13:38:21 -0300

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Matheus Xavier on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
